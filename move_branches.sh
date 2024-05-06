3c767d93e11f533ab7640aa8ce2d89d3514cb949
unitydeveloperor <141172091+unitydeveloperor@users.noreply.github.com>
2024-05-06 15:22:09 +0300

    Create move_branches.sh

---
+++ move_branches.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Move branches with a specific prefix to a folder
+#git branch | grep '^ABC-' | xargs -I branch_name git branch -m branch_name features/branch_name
+
+
+echo "Shell script execution test successful!"
